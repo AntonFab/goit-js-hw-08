8f7ea7aabbe3d0d961350d388e6033d5ebde5cf5
AntonFab <fabyanchuk.anton@gmail.com>
2020-10-17 12:19:24 +0300

    Adds hw-08 decision

---
+++ js/main.js
@@ -0,0 +1,44 @@
+import images from "../gallery-items.js";
+
+const galleryContainer = document.querySelector(".js-gallery");
+
+const galleryElements = images.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+    <a
+        class="gallery__link"
+        href="${original}"
+    >
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</li>`;
+});
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryElements.join(""));
+
+galleryContainer.addEventListener("click", onClick);
+const lightboxContainer = document.querySelector(".js-lightbox");
+const lightboxImage = document.querySelector(".lightbox__image");
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  lightboxContainer.classList.add("is-open");
+  lightboxImage.src = evt.target.dataset.source;
+}
+
+lightboxContainer.addEventListener("click", onModalButtonClose);
+function onModalButtonClose(evt) {
+  console.log(evt.target.dataset.action);
+  if (evt.target.dataset.action === "close-lightbox") {
+    lightboxContainer.classList.remove("is-open");
+    lightboxImage.src = "";
+  }
+}
